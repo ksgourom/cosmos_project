e18888513e065a74880a2d3f710b76d1de357575
Kyriakos <kyriakos.sgourom@gmail.com>
2019-06-06 20:39:26 +0300

    Add app.py, wsgi.py (stub)

---
+++ cosmos_object_detection_api/app.py
@@ -0,0 +1,52 @@
+import argparse
+import io
+import flask
+from utils.object_detection import load_image_as_np_array, get_predictions
+from PIL import Image
+app = flask.Flask(__name__)
+
+
+@app.route('/predict', methods=["POST", "GET"])
+def predict():
+    response = {}
+    if flask.request.method == 'POST':
+        try:
+            image = flask.request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+            img_as_np_arr = load_image_as_np_array(image)
+            predictions = get_predictions(img_as_np_arr)
+            response = {"status": "OK", "success": True, 'predictions': predictions}
+        except (OSError, IOError) as e:
+            response = {"status": "ERROR", "success": False, 'predictions': None, "message": "Error reading image"}
+
+    elif flask.request.method == "GET":
+        response = {"success": False, "message": " please use POST"}
+    return flask.jsonify(response)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Starts the development server. Accepts two argument to determine the host ip address and the port"
+    )
+    parser.add_argument(
+        "--host",
+        required=False,
+        help="The ip address of the host running this server",
+        default='0.0.0.0'
+    )
+    parser.add_argument(
+        "--port",
+        required=False,
+        help="The port the server listens to for incoming connections",
+        default='5000'
+    )
+
+    args = parser.parse_args()
+    app.run(
+        host=args.host,
+        port=args.port,
+    )
+
+
+if __name__ == '__main__':
+    main()
