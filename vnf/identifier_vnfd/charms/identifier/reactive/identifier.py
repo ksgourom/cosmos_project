4f51eb2dd74b20996cca03f2ba7812d1f408c228
George Papathanail <35313995+p4pe@users.noreply.github.com>
2019-06-07 15:21:22 +0300

    Update identifier.py

--- vnf/identifier_vnfd/charms/identifier/reactive/identifier.py
+++ vnf/identifier_vnfd/charms/identifier/reactive/identifier.py
@@ -1,11 +1,19 @@
-from charms.reactive import when, when_not, set_flag, clear_flag
+from charms.reactive import (
+    when, when_not,
+    set_flag,
+    set_state, remove_state
+)
+
 from charmhelpers.core.hookenv import (
-    action_get,
     action_fail,
+    action_get,
     action_set,
+    config,
     status_set,
 )
+
 import charms.sshproxy
+
 
 @when_not('identifier.installed')
 def install_identifier():
@@ -21,10 +29,13 @@ def install_identifier():
     #  * https://github.com/juju-solutions/layer-basic#overview
     #
     set_flag('identifier.installed')
-    status_set('active', 'Ready!')
 
-@when('actions.start-identifier')
-def start-identifier():
+
+cfg = config()
+
+
+@when('config.changed')
+def config_changed():
     err = ''
     try:
         cmd = "echo '' | sudo tee -a /etc/network/interfaces.d/50-cloud-init.cfg > /dev/null && "
@@ -35,72 +46,48 @@ def start-identifier():
     except:
         action_fail('command failed: ' + err)
     else:
-        action_set({'output': result})
-    finally:
-        clear_flag('actions.start-identifier')
+        set_state('identifier.configured')
+        status_set('active', 'ready!')
 
-@when('actions.start')
-def start():
-    # err = ''
-    # ip = action_get('server-ip')
-    # port = action_get('server-port')
-    # try:
-    #     cmd = "/usr/bin/python3 /home/ubuntu/app.py {} {}".format(ip, port)
-    #     result, err = charms.sshproxy._run(cmd)
-    # except:
-    #     action_fail('command failed:' + err)
-    # else:
-    #     action_set({'output': result})
-    # finally:
-    #     clear_flag('actions.start')
-    server_ip = action_get('server-ip')
-    server_port = action_get('server-port')
+
+@when('identifier.configured')
+@when('actions.start_identifier')
+def start_identifier():
+    host = action_get('host')
+    port = action_get('port')
 
     err = ''
     try:
-        cmd = "sudo rm /etc/systemd/system/app.service >/dev/null 2>&1; "
-        cmd += "sudo systemctl stop app.service >/dev/null 2>&1; "
+        cmd = "sudo rm /etc/systemd/system/cosmos.service >/dev/null 2>&1; "
+        cmd += "sudo systemctl stop cosmos.service >/dev/null 2>&1; "
         cmd += "sudo systemctl daemon-reload"
         result, err = charms.sshproxy._run(cmd)
     except:
         action_fail('command failed: ' + err)
-        clear_flag('actions.start')
+        remove_state('actions.start_identifier')
         return
 
     err = ''
     try:
-        cmd = "echo '[Unit]' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo 'Description=app' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo '' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo '[Service]' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo 'Type=simple' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo 'User=ubuntu' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo 'WorkingDirectory=/home/ubuntu' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo 'ExecStart=/usr/bin/python3 app.py {0} {1}' | sudo tee -a /etc/systemd/system/app.service > /dev/null && ".format(server_ip, server_port)
-        cmd += "echo 'Restart=always' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo 'RestartSec=5' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo '' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo '[Install]' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
-        cmd += "echo 'WantedBy=multi-user.target' | sudo tee -a /etc/systemd/system/app.service > /dev/null && "
+        cmd = "echo '[Unit]' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo 'Description=Cosmos' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo '' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo '[Service]' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo 'Type=simple' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo 'User=cosmos' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo 'WorkingDirectory=/home/cosmos' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo 'ExecStart=/usr/bin/python app.py --host {0} --port {1}' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && ".format(host, port)
+        cmd += "echo 'Restart=always' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo 'RestartSec=5' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo '' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo '[Install]' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
+        cmd += "echo 'WantedBy=multi-user.target' | sudo tee -a /etc/systemd/system/cosmos.service > /dev/null && "
         cmd += "sudo systemctl daemon-reload && "
-        cmd += "sudo systemctl start app.service"
+        cmd += "sudo systemctl start cosmos.service"
         result, err = charms.sshproxy._run(cmd)
     except:
         action_fail('command failed: ' + err)
     else:
-        action_set({'output': result})
+        action_set({'output': result, 'errors': err})
     finally:
-        clear_flag('actions.start')
-
-@when('actions.stop')
-def stop():
-    err = ''
-    try:
-        cmd = "sudo systemctl stop app.service"
-        result, err = charms.sshproxy._run(cmd)
-    except:
-        action_fail('command failed:' + err)
-    else:
-        action_set({'output': result})
-    finally:
-        clear_flag('actions.stop')
+        remove_state('actions.start_identifier')
